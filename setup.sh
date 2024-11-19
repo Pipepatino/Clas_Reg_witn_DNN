7c3c03a8ac34b0a34d8b4c6e944102e191361ecb
Pipepatino <felipequimic99@gmail.com>
2024-11-18 19:21:56 -0500

    Comentarios a celdas de codigo para Clasificacion.

--- setup.sh
+++ setup.sh
@@ -1,3 +1,9 @@
-pip3 install --upgrade pip
+pip install --upgrade pip
 pip3 install -r requirements.txt
 pip3 install -e .
+
+pip install -U tensorflow
+pip install protobuf==3.20.*
+
+pip install scikit-learn
+pip install seaborn
